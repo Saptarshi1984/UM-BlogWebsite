d1a9ffca1c102080fb084b6ff72ed59a7baf5691
Saptarshi Mondal <saptarshimondal.mail@gmail.com>
2025-04-14 21:29:05 +0530

    Comment section added

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-function dark_mode() {
+/* function dark_mode() {
 
     const bgcolor = document.querySelector('body');
     
     bgcolor.style.background = "Black";
     bgcolor.style.color = 'white'; 
-}
+} */
 
 function drop_menu () {
 
@@ -15,4 +15,18 @@ function drop_menu () {
     } else {
         x.style.display = "flex";
     }
+}
+
+function submitComment() {
+    const name = document.getElementById("userName").value;
+    const email = document.getElementById("userEmail");
+    const comment = document.getElementById("userComment");
+
+    
+
+    const divElement = document.createElement("div");
+    divElement.innerHTML = `<img src="https://fakeimg.pl/80x80/cccccc/909090" alt="" width="80px" height="80px" style="border-radius: 50%;"><h3>${name}</h3><p>${comment}</p>`;
+
+    document.getElementById("commentBox").appendChild(divElement);
+
 }
