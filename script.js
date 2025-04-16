e61cdc0cc1231fddd5428928787acb8a592d1481
Saptarshi Mondal <saptarshimondal.mail@gmail.com>
2025-04-16 21:12:25 +0530

    Comment section added with like feature, and other styles

--- script.js
+++ script.js
@@ -5,6 +5,8 @@
     bgcolor.style.background = "Black";
     bgcolor.style.color = 'white'; 
 } */
+    let count = 0;
+    
 
 function drop_menu () {
 
@@ -18,15 +20,30 @@ function drop_menu () {
 }
 
 function submitComment() {
+   
+    count++;
     const name = document.getElementById("userName").value;
-    const email = document.getElementById("userEmail");
-    const comment = document.getElementById("userComment");
-
-    
+    const email = document.getElementById("userEmail").value;
+    const comment = document.getElementById("userComment").value;    
 
     const divElement = document.createElement("div");
-    divElement.innerHTML = `<img src="https://fakeimg.pl/80x80/cccccc/909090" alt="" width="80px" height="80px" style="border-radius: 50%;"><h3>${name}</h3><p>${comment}</p>`;
+
+    divElement.innerHTML = `<img src="https://fakeimg.pl/80x80/cccccc/909090" alt="" width="80px" height="80px" style="border-radius: 50%;">
+                            <h3>${name}</h3><p>${comment}</p>                                                       
+                            <i onclick="funcLike(${count})" id="liked-${count}" class="fa-regular fa-thumbs-up"></i>                            
+                            <hr>`;
 
     document.getElementById("commentBox").appendChild(divElement);
+    divElement.classList.add("commentStyle");
+    
+    document.getElementById("userName").value= "";
+    document.getElementById("userEmail").value = "";
+    document.getElementById("userComment").value= "";   
+    
+}
 
+const funcLike = (x) => {
+    
+    document.getElementById(`liked-${x}`).classList.toggle("fa-solid");    
+    
 }
