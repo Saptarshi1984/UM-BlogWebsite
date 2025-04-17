0809edba4ea7d0d3121eb9cbfc1dc5f76f646838
Saptarshi Mondal <saptarshimondal.mail@gmail.com>
2025-04-17 16:15:34 +0530

    new feature in comment section added, reply on comments added

--- script.js
+++ script.js
@@ -5,8 +5,10 @@
     bgcolor.style.background = "Black";
     bgcolor.style.color = 'white'; 
 } */
-    let count = 0;
+   
     
+
+/* Mobile menu related javaScript functions. */
 
 function drop_menu () {
 
@@ -19,9 +21,15 @@ function drop_menu () {
     }
 }
 
+/* Comment section related javaScript functions. */
+
+let count = 0;
+let btnX = 0;
+
 function submitComment() {
    
     count++;
+    btnX++;
     const name = document.getElementById("userName").value;
     const email = document.getElementById("userEmail").value;
     const comment = document.getElementById("userComment").value;    
@@ -29,8 +37,13 @@ function submitComment() {
     const divElement = document.createElement("div");
 
     divElement.innerHTML = `<img src="https://fakeimg.pl/80x80/cccccc/909090" alt="" width="80px" height="80px" style="border-radius: 50%;">
-                            <h3>${name}</h3><p>${comment}</p>                                                       
+                            <h3>@${name}</h3><p>${comment}</p>
+                            <div id="like-reply">                                                      
                             <i onclick="funcLike(${count})" id="liked-${count}" class="fa-regular fa-thumbs-up"></i>                            
+                            <a onclick="replyComment(${count})" id="replyBtn-${count}"><span>Reply</span></a>
+                            </div>
+                            <div id="replies-${count}"></div>
+                            <div id="replySec-${count}" class="inputStyle"></div>     
                             <hr>`;
 
     document.getElementById("commentBox").appendChild(divElement);
@@ -42,8 +55,24 @@ function submitComment() {
     
 }
 
-const funcLike = (x) => {
-    
+const funcLike = (x) => {    
     document.getElementById(`liked-${x}`).classList.toggle("fa-solid");    
-    
 }
+const replyComment = (x) => {    
+    const reply = document.getElementById(`replySec-${x}`);
+    reply.innerHTML = `<input id="replyInput-${x}" type="text"><button id="postBtn-${x}" type="button" onclick="postReply(${x})">Post</button><button id="canBtn-${x}" type="button" onclick="cancel(${x})">Cancel</button>`;
+    document.getElementById(`replySec-${x}`).appendChild(reply);
+}
+const postReply = (x) => {
+const replied = document.getElementById(`replyInput-${x}`).value;
+const rMsg = document.createElement('p');
+rMsg.innerHTML = `<span id="x-${x}">${replied}</span>`;
+document.getElementById(`replies-${x}`).appendChild(rMsg);
+cancel(x);
+}
+const cancel = (x) => {
+     document.getElementById(`replyInput-${x}`).remove();
+     document.getElementById(`postBtn-${x}`).remove(); 
+     document.getElementById(`canBtn-${x}`).remove();   
+}
+
